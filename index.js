493f29b74c9ec513ca72a3e4a4c31e43a268beba
whyucode <dronser8@gmail.com>
2022-06-30 20:16:09 +0530

    fixed script

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const routes = require("./routes/routes");
-require("dotenv").config();
 
 const DATABASE_URL = process.env.DATABASE_URL;
 mongoose.connect(DATABASE_URL);
